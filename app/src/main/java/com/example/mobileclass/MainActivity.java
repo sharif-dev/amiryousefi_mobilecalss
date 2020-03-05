3f83d909521d3feb0d7953111148c63f25c310a2
AmirHossein <amiryousefi98@gmail.com>
2020-03-05 12:03:46 +0330

    solve third issue (remove bad name)

--- app/src/main/java/com/example/mobileclass/MainActivity.java
+++ app/src/main/java/com/example/mobileclass/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.button1);
+        Button button = (Button) findViewById(R.id.btn_toast);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
